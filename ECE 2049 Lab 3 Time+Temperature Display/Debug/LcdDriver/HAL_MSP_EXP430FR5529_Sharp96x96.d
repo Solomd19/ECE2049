ecca1134aea41a1c64a1bb4ec8d54f88c4f18546
Solomd19 <37836137+Solomd19@users.noreply.github.com>
2021-03-10 15:10:14 -0500

    Add files via upload

---
+++ ECE 2049 Lab 3 Time+Temperature Display/Debug/LcdDriver/HAL_MSP_EXP430FR5529_Sharp96x96.d
@@ -0,0 +1,43 @@
+# FIXED
+
+LcdDriver/HAL_MSP_EXP430FR5529_Sharp96x96.obj: ../LcdDriver/HAL_MSP_EXP430FR5529_Sharp96x96.c
+LcdDriver/HAL_MSP_EXP430FR5529_Sharp96x96.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/stdint.h
+LcdDriver/HAL_MSP_EXP430FR5529_Sharp96x96.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/_ti_config.h
+LcdDriver/HAL_MSP_EXP430FR5529_Sharp96x96.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/linkage.h
+LcdDriver/HAL_MSP_EXP430FR5529_Sharp96x96.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/_stdint40.h
+LcdDriver/HAL_MSP_EXP430FR5529_Sharp96x96.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/sys/stdint.h
+LcdDriver/HAL_MSP_EXP430FR5529_Sharp96x96.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/sys/cdefs.h
+LcdDriver/HAL_MSP_EXP430FR5529_Sharp96x96.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/sys/_types.h
+LcdDriver/HAL_MSP_EXP430FR5529_Sharp96x96.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/machine/_types.h
+LcdDriver/HAL_MSP_EXP430FR5529_Sharp96x96.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/machine/_stdint.h
+LcdDriver/HAL_MSP_EXP430FR5529_Sharp96x96.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/sys/_stdint.h
+LcdDriver/HAL_MSP_EXP430FR5529_Sharp96x96.obj: C:/Users/supad/workspace_v10/ECE\ 2049\ Lab\ 3\ Time+Temperature\ Display/grlib/grlib.h
+LcdDriver/HAL_MSP_EXP430FR5529_Sharp96x96.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/stdbool.h
+LcdDriver/HAL_MSP_EXP430FR5529_Sharp96x96.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/assert.h
+LcdDriver/HAL_MSP_EXP430FR5529_Sharp96x96.obj: ../LcdDriver/HAL_MSP_EXP430FR5529_Sharp96x96.h
+LcdDriver/HAL_MSP_EXP430FR5529_Sharp96x96.obj: C:/ti/ccs1020/ccs/ccs_base/msp430/include/msp430.h
+LcdDriver/HAL_MSP_EXP430FR5529_Sharp96x96.obj: C:/ti/ccs1020/ccs/ccs_base/msp430/include/msp430f5529.h
+LcdDriver/HAL_MSP_EXP430FR5529_Sharp96x96.obj: C:/ti/ccs1020/ccs/ccs_base/msp430/include/in430.h
+LcdDriver/HAL_MSP_EXP430FR5529_Sharp96x96.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/intrinsics.h
+LcdDriver/HAL_MSP_EXP430FR5529_Sharp96x96.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/intrinsics_legacy_undefs.h
+
+../LcdDriver/HAL_MSP_EXP430FR5529_Sharp96x96.c: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/stdint.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/_ti_config.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/linkage.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/_stdint40.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/sys/stdint.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/sys/cdefs.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/sys/_types.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/machine/_types.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/machine/_stdint.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/sys/_stdint.h: 
+C:/Users/supad/workspace_v10/ECE\ 2049\ Lab\ 3\ Time+Temperature\ Display/grlib/grlib.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/stdbool.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/assert.h: 
+../LcdDriver/HAL_MSP_EXP430FR5529_Sharp96x96.h: 
+C:/ti/ccs1020/ccs/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccs1020/ccs/ccs_base/msp430/include/msp430f5529.h: 
+C:/ti/ccs1020/ccs/ccs_base/msp430/include/in430.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/intrinsics.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/intrinsics_legacy_undefs.h: 
---
+++ ECE 2049 Lab 3 Time+Temperature Display/Debug/peripherals.d
@@ -0,0 +1,47 @@
+# FIXED
+
+peripherals.obj: ../peripherals.c
+peripherals.obj: ../peripherals.h
+peripherals.obj: C:/ti/ccs1020/ccs/ccs_base/msp430/include/msp430.h
+peripherals.obj: C:/ti/ccs1020/ccs/ccs_base/msp430/include/msp430f5529.h
+peripherals.obj: C:/ti/ccs1020/ccs/ccs_base/msp430/include/in430.h
+peripherals.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/intrinsics.h
+peripherals.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/intrinsics_legacy_undefs.h
+peripherals.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/stdint.h
+peripherals.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/_ti_config.h
+peripherals.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/linkage.h
+peripherals.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/_stdint40.h
+peripherals.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/sys/stdint.h
+peripherals.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/sys/cdefs.h
+peripherals.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/sys/_types.h
+peripherals.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/machine/_types.h
+peripherals.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/machine/_stdint.h
+peripherals.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/sys/_stdint.h
+peripherals.obj: C:/Users/supad/workspace_v10/ECE\ 2049\ Lab\ 3\ Time+Temperature\ Display/grlib/grlib.h
+peripherals.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/stdbool.h
+peripherals.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/assert.h
+peripherals.obj: ../LcdDriver/Sharp96x96.h
+peripherals.obj: ../LcdDriver/HAL_MSP_EXP430FR5529_Sharp96x96.h
+
+../peripherals.c: 
+../peripherals.h: 
+C:/ti/ccs1020/ccs/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccs1020/ccs/ccs_base/msp430/include/msp430f5529.h: 
+C:/ti/ccs1020/ccs/ccs_base/msp430/include/in430.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/intrinsics.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/intrinsics_legacy_undefs.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/stdint.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/_ti_config.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/linkage.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/_stdint40.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/sys/stdint.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/sys/cdefs.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/sys/_types.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/machine/_types.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/machine/_stdint.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/sys/_stdint.h: 
+C:/Users/supad/workspace_v10/ECE\ 2049\ Lab\ 3\ Time+Temperature\ Display/grlib/grlib.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/stdbool.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.12.0.STS/include/assert.h: 
+../LcdDriver/Sharp96x96.h: 
+../LcdDriver/HAL_MSP_EXP430FR5529_Sharp96x96.h: 
